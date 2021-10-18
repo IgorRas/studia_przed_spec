640a31e5a7f55222a7b75d62b043522f8d1d466b
IgorRas <92763331+IgorRas@users.noreply.github.com>
2021-10-19 01:26:35 +0200

    Add files via upload

---
+++ Eratostates.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <iostream>
+
+//Sito Eratostenesa z dynamiczn¹ alokacj¹ pamiêci
+
+int main (int argc, char *argv[])	{
+	int n = atoi(argv[1]);
+	int s=0;
+	int *p=(int*)std::malloc(n * sizeof(int));
+	for (int j=2; j<n; j++)	{
+		p[j]=1;	
+	}
+	for (int j=2; j<n; j++)	{
+		for (int w=j+j; w<=n; w+=j)	{
+			p[w]=0;
+		}
+	}
+	for (int j=2; j<n; j++)	{
+		if(p[j]==1)	{
+			std::cout<<j<<" ";
+		}
+	}
+	std::free(p);
+}
