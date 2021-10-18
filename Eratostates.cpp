ffd14bf972da823ca34745db7e9fe9ed570db599
IgorRas <92763331+IgorRas@users.noreply.github.com>
2021-10-19 01:27:07 +0200

    Update Eratostates.cpp

--- Eratostates.cpp
+++ Eratostates.cpp
@@ -1,7 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 
-//Sito Eratostenesa z dynamiczn¹ alokacj¹ pamiêci
+//Sito Eratostenesa z dynamiczna alokacja pamieci
 
 int main (int argc, char *argv[])	{
 	int n = atoi(argv[1]);
